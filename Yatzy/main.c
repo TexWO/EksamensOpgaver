bb8dba16e7d62fec3b23ce5aac479b8f03f6938a
Simon Woidemann <Simonwoidemann@gmail.com>
2023-11-19 18:43:13 +0100

    New and improved version

--- Yatzy/main.c
+++ Yatzy/main.c
@@ -2,29 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-void roll_multiple_dice() {
+void roll_multiple_dice(int arr[], int num_of_dice) {
   /* Make a function that rolls multiple dice and store them in an int array */
-
-  time_t seed;
-  seed = time(NULL);
-  srand(seed);
-  int num_of_dice;
-
-  printf("How many dice do you want to roll?\n");
-  scanf("%d", &num_of_dice);
-
-  int arr[num_of_dice];
 
   for (int i = 0; i < num_of_dice; i++) {
     arr[i] = rand() % 6 + 1;
   }
-
-  printf("You rolled: ");
-  for (int i = 0; i < num_of_dice; i++) {
-    printf("%d ", arr[i]);
-  }
 }
 
+
+void print_dice(int arr[], int num_of_dice, int num_of_sides) {
+  /* Make a function that prints dice */
+  printf("%d-ere", num_of_sides);
+  for (int i = 0; i < num_of_dice; i++) {
+    printf(" %d", arr[i]);
+  }
+  printf("\n");
+}
 
 int scoreboard() {
   /* Make a function that keeps track of the score */
@@ -33,6 +27,35 @@ int scoreboard() {
 
 
 int main() {
-  roll_multiple_dice();
+  time_t seed;
+  seed = time(NULL);
+  srand(seed);
+
+  int num_of_dice = 0;
+
+  printf("Yatzy with how many dice? (a number under 5 terminates)");
+  scanf("%d", &num_of_dice);
+
+  if(num_of_dice < 5) {
+    printf("Terminating program\n");
+    return 1;
+  }
+
+  int dice[num_of_dice];
+  int num_of_sides = 1;
+
+  printf("Printing dies:\n");
+  while(1) {
+    roll_multiple_dice(dice, num_of_dice);
+    print_dice(dice, num_of_dice, num_of_sides);
+    printf("\n");
+
+    num_of_sides++;
+
+    if(num_of_sides > 6) {
+      break;
+    }
+  }
+
   return 0;
 }
